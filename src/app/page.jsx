833f40d48cc9b757197bbafd2149598f99439f43
unknown <david.rowlands@live.com>
2025-04-01 12:51:25 +0900

    Changed title back

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-900 text-gray-300">
       <div className="container mx-auto p-6 sm:p-12">
           <h1 className="text-5xl md:text-6xl font-extrabold text-white mb-6">
-              WATCH LIST!!!!!!
+              Welcome to the watchlist website
           </h1>
           <p className="text-lg md:text-xl text-white mb-6">
               Your personal space to curate and manage a wishlist of your favorite watches. 
